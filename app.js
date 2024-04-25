6742b66a5461a11dc9e457a46b4db88f31bc1ce8
Chetan Bedakihale <152062682+Chetan-B9@users.noreply.github.com>
2024-04-25 10:15:06 +0530

    ADDED: Clear weather option in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cityMessage = document.querySelector('.city-message');
 
 // weather icons 
 const weatherIcons = {
+   Clear: "Weather Icons/Icons type 2/sun.png",
    Clouds: "Weather Icons/Icons type 2/sun-cloud.png",
    Rain: "Weather Icons/Icons type 2/rain.png",
    Snow: "Weather Icons/Icons type 2/snow-cloud.png",
